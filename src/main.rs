ed16f5e77e4812ad379a41ad6c7807367511d28f
grazzgit <github@grazzhopper.maskmy.id>
2023-12-30 16:01:34 +0100

    basic command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    name: Option<String>,
+
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    #[arg[short, long, action = clap::ArgAction::Count]]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Test {
+        #[arg(short, long)]
+        list: bool,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    if let Some(name) = cli.name.as_deref() {
+        println!("Hello, {}!", name);
+    }
 }
