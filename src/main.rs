1964200ac48eb16eb4a7681fe558874e1bd5b9c6
grazzgit <github@grazzhopper.maskmy.id>
2023-12-31 15:57:33 +0100

    completing first lesson

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
+use std::io;
+
 fn main() {
-    let mut mars_weight = calculate_our_weight_on_mars(75.0);
-    mars_weight = mars_weight * 1000.0;
-    println!("Our weight on Mars is {} grams.", mars_weight);
+    println!("Enter your weight on Earth: ");
+    let earth_weight = get_earth_weight_input();
+    let mars_weight = calculate_our_weight_on_mars(earth_weight);
+    println!("Your weight on Mars is {} grams.", mars_weight);
 }
 
 fn calculate_our_weight_on_mars(weight: f32) -> f32 {
-    // calculate our weight on mars
     weight / 9.81 * 3.711
+}
+
+fn get_earth_weight_input() -> f32 {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    input.trim().parse().unwrap()
 }
