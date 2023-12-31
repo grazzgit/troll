2acd85ae339af97634f595a12ee8f20ddb2eb5fc
grazzgit <github@grazzhopper.maskmy.id>
2023-12-31 15:07:17 +0100

    udemy course code

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
-struct User {
-    name: String,
-    email: String,
-}
-
-impl User {
-    fn new(name: &str) -> User {
-        return User {
-            name: name.to_string(),
-            email: format!("{}@example.com", name),
-        };
-    }
-}
-
 fn main() {
-    let user = User::new("John");
-    println!("user:{}, email:{}", user.name, user.email)
+    let mut mars_weight = calculate_our_weight_on_mars(75.0);
+    mars_weight = mars_weight * 1000.0;
+    println!("Our weight on Mars is {} grams.", mars_weight);
+}
+
+fn calculate_our_weight_on_mars(weight: f32) -> f32 {
+    // calculate our weight on mars
+    weight / 9.81 * 3.711
 }
